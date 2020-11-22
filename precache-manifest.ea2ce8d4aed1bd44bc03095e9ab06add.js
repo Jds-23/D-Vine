93508a139a9a1093b0bf4133726dedf003e07d8a
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-11-21 21:59:34 -0800

    Updates

---
+++ precache-manifest.ea2ce8d4aed1bd44bc03095e9ab06add.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee381b3c2fa1a1c7b3d7b079c70345c6",
+    "url": "/D-Vine/index.html"
+  },
+  {
+    "revision": "7a5a54d0e6ce33ab3d44",
+    "url": "/D-Vine/static/css/main.ee61c5f0.chunk.css"
+  },
+  {
+    "revision": "c349598cdeaa1bd6d8ca",
+    "url": "/D-Vine/static/js/2.f7376910.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/D-Vine/static/js/2.f7376910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5a54d0e6ce33ab3d44",
+    "url": "/D-Vine/static/js/main.a4edc0d7.chunk.js"
+  },
+  {
+    "revision": "5528ba10517a9b0d2f33",
+    "url": "/D-Vine/static/js/runtime-main.ad0b152e.js"
+  },
+  {
+    "revision": "1820dd825cddf5ef447c4d31f301ee01",
+    "url": "/D-Vine/static/media/Jumbotron-background.1820dd82.jpg"
+  },
+  {
+    "revision": "4b82e66dbb273befd9c95dd2e52750d6",
+    "url": "/D-Vine/static/media/Logo.4b82e66d.png"
+  },
+  {
+    "revision": "b762277a52180eddeb6371319c53723f",
+    "url": "/D-Vine/static/media/Logo.b762277a.gif"
+  },
+  {
+    "revision": "f8dd75e61c7faaacfa16a91c09d019bf",
+    "url": "/D-Vine/static/media/news-background.f8dd75e6.jpg"
+  },
+  {
+    "revision": "b6250faa2fd2b178809861aeefda02cb",
+    "url": "/D-Vine/static/media/search.b6250faa.png"
+  },
+  {
+    "revision": "c7ff7c8d6ca37237d34f0d2bce5f0532",
+    "url": "/D-Vine/static/media/star-empty.c7ff7c8d.png"
+  },
+  {
+    "revision": "c625b2ceb3b7719a17e660ddb17a8534",
+    "url": "/D-Vine/static/media/star-filled.c625b2ce.png"
+  },
+  {
+    "revision": "e631b64bf4b113775580d794c02ac981",
+    "url": "/D-Vine/static/media/wine_placeholder.e631b64b.png"
+  }
+]);
